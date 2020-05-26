6ac12841afba7421876822cd9675a3d4e390d536
rafaelpntfs@gmail.com <Rafaelpntfs@gmail.com>
2020-05-25 20:47:09 -0400

    Uploading prototype game 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //This is being used to store the INPUT.GETAXIS variables from the input manager
+    public float horizontalInput;
+    //The speed at which our player will move from left to right
+    public float speed = 10.0f;
+
+    //public float leftBoundary = -10.0f;
+    //public float rightBoundary = 10.0f;
+
+    public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        //here we are assigning the variable equal to the Input manager. From here
+        // you may assign inputs from the input manager. In this case Horizontal will offer 
+        // horizontal input == -1 or +1 which means our character whill move left or right.
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //Here we begin to translate the position of the player from VECTOR 3 RIGHT and multiply it by time so that its not ever 60 fps
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+
+        //iF THE SPACE KEY IS PRESSED DOWN SOMETHING WILL HAPPEN
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Instantiate is another way of saying were going to create an object.
+            // Instantiate is for creating copes for objects that already exist.
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // An array of game objects that we will call array
+    public GameObject[] animalPrefabs;
+    //variable to store the animals X spawn position.
+    private float spawnRangeX = 20;
+
+    //Variable to store the animals z spawn position.
+    private float spawnPosZ = 20;
+    //Variable for delaying spawn of animal
+    private float startDelay = 2;
+    //Variable to invoke repeating every 1.5 seconds.
+    private float spawnInterval = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Unity Method. Takes a method we want to call, and at a certain time will start to call that method and then constantly repeat it.
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.S))
+        // {
+        //     //everytime you press S it will call the SpawnRandomAnimal method/function.
+        //     SpawnRandomAnimal();
+        // }
+
+    }
+
+
+    //Everytime we call the spawn animal function it will go through these lines of code until the end and spawn random animals
+    void SpawnRandomAnimal()
+    {
+        //This will be the index to find the different animals in our array to spawn
+        //Using random to select which animal you want to choose.
+        //this variable only needs to be seen in this method.
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        //Create a vector 3 variable to store coordinates of animals
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
